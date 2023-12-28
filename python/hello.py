f5d8577b007244c8f05b35172319d3948cae8827
RadiantXD <joushuasmall@gmail.com>
2023-12-28 14:57:40 -0400

    modified:   python/hello.py

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,19 @@
-print("Hello, world!")
+#Decorators
+
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+@announce
+def hello():
+    print("Hello, world!")
+
+def main():
+    hello()
+    
+
+if __name__ == '__main__':
+    main()
